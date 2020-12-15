898bffe40f0d58dc0c475a6895d26fdbf7242800
Philip Carlson <writephilcarlson@gmail.com>
2020-12-15 13:01:33 -0500

    Redesigns evaluateRegex method
    This is the first in what I think will be a series of changes.
    I plan on changing all three of the methods that filter lists to have then filter only strings.
    Then I can call them in a single stream from the main method.
    I think it will be easier to debug!

--- src/com/tunepruner/main/Main.java
+++ src/com/tunepruner/main/Main.java
@@ -19,35 +19,55 @@ public class Main {
 //        String lettersOnBoard = ""
 
         /*
-        * User enters preferred connecting letter,
-        * then enters jackpot indexes in positive or negative values,
-        * relative to the connecting letter.
-        *
-        * And then enters secondary connecting letter options in the same way.
-        *
-        * User enters all 7 (or less if end of game) characters.
-        * Program know which are most valuable.
-        *
-        * Program returns collection of possible plays.
-        * They don't necessarily use all 7 characters,
-        * but highest value possible, counting points of all letters used,
-        * even taking into account the jackpot indexes entered by user.
-        * */
+         * User enters preferred connecting letter,
+         * then enters jackpot indexes in positive or negative values,
+         * relative to the connecting letter.
+         *
+         * And then enters secondary connecting letter options in the same way.
+         *
+         * User enters all 7 (or less if end of game) characters.
+         * Program know which are most valuable.
+         *
+         * Program returns collection of possible plays.
+         * They don't necessarily use all 7 characters,
+         * but highest value possible, counting points of all letters used,
+         * even taking into account the jackpot indexes entered by user.
+         * */
 
-        ArrayList<String> finalList = testForLength(lookForRepeatCharacters(evaluateRegex(regex)), 7);
+//        ArrayList<String> finalList = testForLength(lookForRepeatCharacters(evaluateRegex(regex)), 7);
 
     }
 
-    private static ArrayList<String> testForLength(List<String> listPassedIn, int requestedLength) {
-        ArrayList<String> listToReturn = new ArrayList<String>();
-        listPassedIn.stream().filter(string -> {
-            return string.length() <= requestedLength;
-        }).forEach(listToReturn::add);
+    private static void importWholeDictionary(List<String> listToAddDictionaryTo) throws IOException {
+        File file = new File("EnglishWords.txt");
+        try {
+            file.createNewFile();
+        } catch (
+                IOException e) {
+            System.out.println("That file already exists");
+        }
+        BufferedReader bufReader = new BufferedReader(new FileReader(file.getAbsolutePath()));
+        String line;
+        line = bufReader.readLine();
 
-        System.out.printf("%s are within the acceptable length.\n", listToReturn.size());
-        System.out.println("Here they are: ");
-        System.out.println(listToReturn);
-        return listToReturn;
+        try {
+            while (line != null) {
+                listToAddDictionaryTo.add(line);
+                line = bufReader.readLine();
+            }
+        } catch (IOException f) {
+            f.printStackTrace();
+        }
+        bufReader.close();
+
+        System.out.printf("There are %d words total in this dictionary.\n", listToAddDictionaryTo.size());
+    }
+
+    private static boolean evaluateRegex(String regex) {
+        Pattern pattern = Pattern.compile(regex);
+        Matcher matcher = pattern.matcher(regex);
+        return matcher.find();
+
     }
 
     private static ArrayList<String> lookForRepeatCharacters(ArrayList<String> listPassedIn) {
@@ -76,43 +96,18 @@ public class Main {
         return listToReturn;
     }
 
-    private static ArrayList<String> evaluateRegex(String regex) {
-        ArrayList<String> list = new ArrayList<>();
-        listOfWords
-                .stream()
-                .filter(string -> {
-                    Pattern pattern = Pattern.compile(regex);
-                    Matcher matcher = pattern.matcher(string);
-                    return matcher.find();
-                })
-                .forEach(string -> list.add(string));
-//
-        System.out.printf("There are %s matches from the regex.\n", list.size());
-        return list;
+    private static ArrayList<String> testForLength(List<String> listPassedIn, int requestedLength) {
+        ArrayList<String> listToReturn = new ArrayList<String>();
+        listPassedIn.stream().filter(string -> {
+            return string.length() <= requestedLength;
+        }).forEach(listToReturn::add);
+
+        System.out.printf("%s are within the acceptable length.\n", listToReturn.size());
+        System.out.println("Here they are: ");
+        System.out.println(listToReturn);
+        return listToReturn;
     }
 
-    private static void importWholeDictionary(List<String> listPassedIn) throws IOException {
-        File file = new File("EnglishWords.txt");
-        try {
-            file.createNewFile();
-        } catch (
-                IOException e) {
-            System.out.println("That file already exists");
-        }
-        BufferedReader bufReader = new BufferedReader(new FileReader(file.getAbsolutePath()));
-        String line;
-        line = bufReader.readLine();
-
-        try {
-            while (line != null) {
-                listPassedIn.add(line);
-                line = bufReader.readLine();
-            }
-        } catch (IOException f) {
-            f.printStackTrace();
-        }
-        bufReader.close();
-
-        System.out.printf("There are %d words total in this dictionary.\n", listPassedIn.size());
-    }
+    private static ArrayList<String>
 }
+
